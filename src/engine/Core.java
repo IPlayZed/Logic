1a0ea87238ec289674d5f06559cf3951bed1d77a
IPlayZed <pauljouser@gmail.com>
2021-01-29 19:58:59 +0100

    The loop updates the window in each iteration to the given loop.
    Signed-off-by: IPlayZed <pauljouser@gmail.com>

--- src/engine/Core.java
+++ src/engine/Core.java
@@ -11,14 +11,19 @@ import static org.lwjgl.glfw.GLFW.*;
  */
 public class Core {
     public static void main(String[] args) {
-        long window=App.Init(); //create the window
+        long window = App.Init(); //create the window
+
+        //does not need to be this set color, just for demonstration purposes
+        Color clearColor = Color.yellow();
 
         //as long as window close state is false loop events
         while (!glfwWindowShouldClose(window)) {
             glfwPollEvents(); //pool events for window
 
+            //see https://docs.gl/gl4/glClear
             GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-            GL11.glClearColor(0, 0, 0, 1);
+            GL11.glClearColor(clearColor.getR(), clearColor.getG(),
+                    clearColor.getB(), 1);
 
             glfwSwapBuffers(window);
         }
